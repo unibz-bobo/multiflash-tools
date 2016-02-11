407c4a98a24b75d994c03ebbd3f5326425b3493f
Julian Sanin <sanin89julian@gmail.com>
2016-02-11 11:47:06 +0100

    Added prototype of the sender

---
+++ multiflash.lib.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Utilities
+getNetworkIp() {
+  local interface=$1
+  local interfaceInfo=(`ip -o -4 addr list $interface`)
+  local network=${interfaceInfo[3]}
+  local ip=`echo $network | cut -f1 -d "/"`
+  echo "$ip"
+}
+
+getNetworkIp6() {
+  local interface=$1
+  local interfaceInfo=(`ip -o -4 addr list $interface`)
+  local network=${interfaceInfo[3]}
+  local ip=`echo $network | cut -f1 -d "/"`
+  echo "$ip"
+}
+
+# Default Values
+programName="$(basename $0)"
+defaultInterface="eth0"
+message="multiflash-debug"
+networkIp=`getNetworkIp $defaultInterface`
+networkIp6=`getNetworkIp6 $defaultInterface`
+timeIntervalInSeconds=1
+multicastGroupIp="226.1.3.14"
+multicastGroupPort="31415"
+
